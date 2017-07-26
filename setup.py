0b1a363ab28cc0368cdb3d4c5e2aae427d3c7abd
vetains <vetains@163.com>
2017-07-26 11:25:41 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='BMAssistant',
       version='1.3',
       description='A bookmark assistant',
       author='vetains',
-      author_email='vetains@126.com',
+      author_email='vetains@163.com',
       py_modules=['BMAssistant'],
       console=['BMAssistant.py'],
       windows=["BMAssistant.py"],options = { "py2exe":{"dll_excludes":["MSVCP90.dll"]}})
